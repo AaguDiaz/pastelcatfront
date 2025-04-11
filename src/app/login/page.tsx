fd03850f428ed875eb3480a9eb459d70af5b7936
Agustin <aaagusrc@gmail.com>
2025-04-11 19:37:30 -0300

    inicio y cierre de sesion

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,41 +1,86 @@
+'use client';
+
+import {useState} from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
-  return (
-    <div className="min-h-screen flex items-start bg-pastel-cream justify-center pt-16 ">
-        <div className="w-full max-w-md p-8 bg-pastel-beige shadow-2xl rounded-lg ">
-          <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
-          <form className="space-y-4">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium ">Correo</label>
-              <input
-                id="email"
-                type="email"
-                className="w-full p-2 mt-1 border rounded focus:outline-none focus:ring-2 focus:ring-pastel-blue"
-                placeholder="tu@correo.com"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="block text-sm font-medium">Contraseña</label>
-              <input
-                id="password"
-                type="password"
-                className="w-full p-2 mt-1 border rounded focus:outline-none focus:ring-2 focus:ring-pastel-blue"
-                placeholder="••••••••"
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full py-2 bg-pastel-blue rounded-lg hover:bg-blue-500 transition"
-            >
-              Entrar
-            </button>
-          </form>
-          <p className="mt-2 text-sm text-center ">
-            ¿No tienes cuenta? <Link href="#" className="text-blue-500 hover:underline">Regístrate</Link>
-          </p>
-        </div>
-      
-    </div>
-  );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+
+    try{
+      const res = await fetch('https://pastelcatback.vercel.app/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email, password})
+      });
+      const data = await res.json();
+
+      if(!res.ok){
+        throw new Error(data.message || "Error al iniciar sesión");
+      }
+      localStorage.setItem("token", data.session.access_token);
+      router.push("/dashboard");
+    }catch (err: any){
+      setError(err.message);
+    }finally{
+      setLoading(false);
+    }
+  };
+    return (
+      <div className="min-h-screen flex items-start bg-pastel-cream justify-center pt-16 ">
+          <div className="w-full max-w-md p-8 bg-pastel-beige shadow-2xl rounded-lg ">
+            <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
+            <form className="space-y-4" onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium ">Correo</label>
+                <input
+                  id="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full p-2 mt-1 border rounded focus:outline-none focus:ring-2 focus:ring-pastel-blue"
+                  placeholder="tu@correo.com"
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="block text-sm font-medium">Contraseña</label>
+                <input
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full p-2 mt-1 border rounded focus:outline-none focus:ring-2 focus:ring-pastel-blue"
+                  placeholder="••••••••"
+                  required
+                />
+              </div>
+              {error && <p className="text-red-500 text-sm text-center">{error}</p>}
+              <button
+                type="submit"
+                disabled={loading}
+                className="w-full py-2 bg-pastel-blue rounded-lg hover:bg-blue-500 transition"
+              >
+                {loading ? "Cargando..." : "Iniciar Sesión"}
+              </button>
+            </form>
+            <p className="mt-2 text-sm text-center ">
+              ¿No tienes cuenta? <Link href="#" className="text-blue-500 hover:underline">Regístrate</Link>
+            </p>
+          </div>
+        
+      </div>
+    );
+  
 }
